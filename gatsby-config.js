6a18187193cdf9a187478f82ee160dbb7dc7570e
MokiMo <bannedjobs@gmail.com>
2018-12-21 14:39:38 +0100

    fix(setup): configure netlify together with livingdocs-magazine

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,9 +9,7 @@ module.exports = {
       options: {
         limit: 50, // defaults to 10, if there is none
         // The accessToken is accessed at buildtime and set in netlify in this case
-        accessToken:
-          // eslint-disable-next-line max-len
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6InB1YmxpYy1hcGk6cmVhZCIsIm5hbWUiOiJMaXZpbmdkb2NzLW9uYm9hcmRpbmciLCJwcm9qZWN0SWQiOjExMSwiY2hhbm5lbElkIjoxMDgsInR5cGUiOiJjbGllbnQiLCJqdGkiOiJlMzAzYWM2OS1hOTQwLTQ4NjYtOTViYS01ZDdkNDk3NDgwMTkiLCJjb2RlIjoiZTMwM2FjNjktYTk0MC00ODY2LTk1YmEtNWQ3ZDQ5NzQ4MDE5IiwiaWF0IjoxNTQxMDg0OTQxfQ.jB0ipjwA0hgxD1bxxZSzHIKbyw1W-swOKe8hbrScBmo'
+        accessToken: process.env.accessToken
       }
     },
     'gatsby-plugin-react-helmet',
