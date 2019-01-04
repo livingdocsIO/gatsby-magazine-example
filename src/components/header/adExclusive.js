8226291195dfe815712d0ff7c379f541a13bfd9d
MokiMo <bannedjobs@gmail.com>
2019-01-04 15:45:23 +0100

    fix(links): use <a/> tags for external links

--- src/components/header/adExclusive.js
+++ src/components/header/adExclusive.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import {Link} from 'gatsby'
 import ad from '../resources/ad-exclusive.gif'
 
 const adExclusive = () => (
   <div className="ad ad--head">
-    <Link to="https://www.livingdocs.io" target="_blank" rel=" noopener noreferrer">
+    <a href="https://www.livingdocs.io" target="_blank" rel=" noopener noreferrer">
       <img alt="test" src={ad} style={{maxHeight: '90px'}} />
-    </Link>
+    </a>
   </div>
 )
 
