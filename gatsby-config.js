2dd37fc9e15af5e27bcc90e26d291ef1aa8e20d6
mokimo <bannedjobs@gmail.com>
2019-02-14 16:00:16 +0100

    fix(config): use correct urls, add TODO's for end users

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,19 @@
 module.exports = {
   // siteMetadata is used for sitemap.xml
   siteMetadata: {
-    siteUrl: `https://www.living-times.com/`
+    siteUrl: `https://magazine-example.livingdocs.io/sitemap.xml` // @TODO swap out for your host
   },
   plugins: [
     {
       resolve: 'gatsby-source-livingdocs',
       options: {
-        limit: 50, // defaults to 10, if there is none
         // The accessToken is accessed at buildtime and set in netlify in this case
         accessToken: process.env.accessToken
+
+        // limit: 10, Default=10. Max=100. Defines how many documents are gathered per request.
+        // recursion: true, Default=true changing not advised.
+        // recursion: false - limit will now be the number of documents you recieve back.
+
       }
     },
     'gatsby-plugin-react-helmet',
@@ -17,14 +21,11 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
-        host: 'https://www.living-times.com/', // @TODO add Link
-        sitemap: 'https://www.living-times.com/sitemap.xml', // @TODO add Link
+        host: 'https://magazine-example.livingdocs.io/', // @TODO swap out for your host
+        sitemap: 'https://magazine-example.livingdocs.io/sitemap.xml', // @TODO swap out for your host
         env: {
           development: {
             policy: [{userAgent: '*', disallow: ['/']}]
-          },
-          production: {
-            policy: [{userAgent: '*', disallow: '/'}] // @TODO change to allow
           }
         }
       }
@@ -32,7 +33,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'living-times',
+        name: 'living-times', // @TODO swap out for your name / icon
         short_name: 'living-times',
         start_url: '/',
         background_color: '#6b37bf',
@@ -44,7 +45,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-sass`,
       options: {
-        includePaths: ['src/resources/stylesheets/living-times.scss']
+        includePaths: ['src/resources/stylesheets/living-times.scss'] // @TODO swap out for your stylesheets
       }
     }
   ]
